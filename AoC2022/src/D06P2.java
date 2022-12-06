deb3856af7e1723710ca83d30fb4b8793151be18
tluo5458 <tluo5@DESKTOP-8IGITJ6.mynetworksettings.com>
2022-12-06 00:09:06 -0500

    Day 6 solution

---
+++ AoC2022/src/D06P2.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.HashSet;
+
+public class D06P2 {
+	@SuppressWarnings("resource")
+	public static int ans() {
+		int ans = 0;
+		
+		File file = new File("inputs/d-06.txt");
+
+		BufferedReader br;
+		String st;
+		try {
+			br = new BufferedReader(new FileReader(file));
+			while ((st = br.readLine()) != null) {
+				// we do a bit of parsing
+				for (int i = 0; i < st.length(); i++) {
+					HashSet<Character> set = new HashSet<Character>();
+					for (int j = i; j < i + 14; j++) {
+						set.add(st.charAt(j));
+					}
+					if (set.size() == 14) {
+						return i + 14;
+					}
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return ans;
+	}
+	
+	public static int[] toInt(String[] s) {
+		int[] ret = new int[s.length];
+		for (int i = 0; i < s.length; i++) {
+			ret[i] = Integer.parseInt(s[i]);
+		}
+		return ret;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(ans());
+	}
+}
