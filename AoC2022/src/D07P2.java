e1246c561bb1aa1e86bcbc8a8cbcc84e879fc31b
tluo5458 <tluo5@DESKTOP-8IGITJ6.mynetworksettings.com>
2022-12-07 00:35:04 -0500

    Day 7 solve

---
+++ AoC2022/src/D07P2.java
@@ -0,0 +1,114 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.HashMap;
+
+public class D07P2 {
+	private static class Node {
+		HashMap<String, Node> children;
+		Node parent;
+		int size;
+		
+		Node() {
+			children = new HashMap<String, Node>();
+			size = 0;
+			parent = null;
+		}
+		
+		Node(Node n) {
+			children = new HashMap<String, Node>();
+			size = 0;
+			parent = n;
+		}
+		
+		public void addNode(String s) {
+			children.put(s, new Node(this));
+		}
+		
+		public void addFile(String s, int size) {
+			children.put(s, new Node(this));
+			children.get(s).size = size;
+		}
+		
+		public int totalSize() {
+			int ret = size;
+			for (String s : children.keySet()) {
+				ret += children.get(s).totalSize();
+			}
+			return ret;
+		}
+		
+		public int minAbove(int thresh) {
+			int currBest = Integer.MAX_VALUE;
+			if (children.size() == 0) {
+				return currBest;
+			}
+			int curr = this.totalSize();
+			if (currBest > curr && curr >= thresh) {
+				currBest = curr;
+			}
+			for (Node n : children.values()) {
+				int ncurr = n.minAbove(thresh);
+				if (currBest > ncurr && ncurr >= thresh) {
+					currBest = ncurr;
+				}
+			}
+			return currBest;
+		}
+	}
+	
+	public static int ans() {
+		File file = new File("inputs/d-07.txt");
+
+		Node head = new Node();
+		Node curr = head;
+		
+		BufferedReader br;
+		String st;
+		try {
+			br = new BufferedReader(new FileReader(file));
+			while ((st = br.readLine()) != null) {
+				// we do a bit of parsing
+				String[] parse = st.split(" ");
+				if (parse[0].equals("$")) {
+					// is a command
+					if (parse[1].equals("cd")) {
+						if (parse[2].equals("/")) {
+							curr = head;
+						} else if (parse[2].equals("..")) {
+							curr = curr.parent;
+						} else {
+							if (!curr.children.containsKey(parse[2])) {
+								curr.addNode(parse[2]);
+							}
+							curr = curr.children.get(parse[2]);
+						}
+					} else if (parse[1].equals("ls")) {
+						// do nothing
+					}
+				} else {
+					if (parse[0].equals("dir")) {
+						curr.addNode(parse[1]);
+					} else {
+						curr.addFile(parse[1], Integer.parseInt(parse[0]));
+					}
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return head.minAbove(head.totalSize() - 40000000);
+	}
+	
+	public static int[] toInt(String[] s) {
+		int[] ret = new int[s.length];
+		for (int i = 0; i < s.length; i++) {
+			ret[i] = Integer.parseInt(s[i]);
+		}
+		return ret;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(ans());
+	}
+}
