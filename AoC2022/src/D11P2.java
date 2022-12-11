aa1a0ac2c3a8bd47d33743959ec316cdd576bf07
tluo5458 <tluo5@DESKTOP-8IGITJ6.mynetworksettings.com>
2022-12-11 02:40:12 -0500

    Day 11 solve

---
+++ AoC2022/src/D11P2.java
@@ -0,0 +1,124 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class D11P2 {
+	private static class Monkey {
+		ArrayList<Long> items;
+		int test;
+		int tThrow;
+		int fThrow;
+		char op;
+		int opNum;
+		int ct = 0;
+		
+		public Monkey() {
+			items = new ArrayList<Long>();
+		}
+		
+		public String toString() {
+			return items.toString() + " ct " + ct + " div " + test + " tru " + tThrow + " fal " + fThrow + " op " + op + " " + opNum;
+		}
+	}
+	
+	public static long ans() {
+		long ans = 0;
+		
+		File file = new File("inputs/d-11.txt");
+
+		ArrayList<Monkey> monkeys = new ArrayList<Monkey>();
+		
+		BufferedReader br;
+		String st;
+		try {
+			br = new BufferedReader(new FileReader(file));
+			Monkey temp = new Monkey();
+			while ((st = br.readLine()) != null) {
+				// we do a bit of parsing
+				String[] parse = st.trim().split(" ");
+				if (parse[0].equals("Monkey")) {
+					temp = new Monkey();
+				} else if (st.trim().equals("")) {
+					monkeys.add(temp);
+				} else if (parse[0].equals("Starting")) {
+					int[] mItem = toInt(st.trim().substring(16).split(", "));
+					for (int i : mItem) {
+						temp.items.add((long) i);
+					}
+				} else if (parse[0].equals("Operation:")) {
+					if (parse[5].equals("old")) {
+						temp.op = '^';
+						temp.opNum = 2;
+					} else {
+						temp.op = parse[4].charAt(0);
+						temp.opNum = Integer.parseInt(parse[5]);
+					}
+				} else if (parse[0].equals("Test:")) {
+					temp.test = Integer.parseInt(parse[3]);
+				} else if (parse[1].equals("true:")) {
+					temp.tThrow = Integer.parseInt(parse[5]);
+				} else if (parse[1].equals("false:")) {
+					temp.fThrow = Integer.parseInt(parse[5]);
+				} else {
+					System.out.println("Very bad " + st);
+				}
+			}
+			monkeys.add(temp);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		long mod = 1;
+		for (Monkey m : monkeys) {
+			mod *= m.test;
+		}
+		
+		for (int i = 0; i < 10000; i++) {
+			for (int m = 0; m < monkeys.size(); m++) {
+				Monkey c = monkeys.get(m);
+				int num = c.items.size();
+				for (int j = 0; j < num; j++) {
+					long item = c.items.remove(0);
+					if (c.op == '*') {
+						item *= c.opNum;
+					} else if (c.op == '+') {
+						item += c.opNum;
+					} else if (c.op == '^') {
+						item *= item;
+					} else {
+						System.out.println("Stinky op " + c.op);
+					}
+					item = item % mod;
+					if (item % c.test == 0) {
+						monkeys.get(c.tThrow).items.add(item);
+					} else {
+						monkeys.get(c.fThrow).items.add(item);
+					}
+					c.ct++;
+				}
+			}
+		}
+		
+		long[] active = new long[monkeys.size()];
+		for (int i = 0; i < monkeys.size(); i++) {
+			active[i] = (long) monkeys.get(i).ct;
+		}
+		Arrays.sort(active);
+		ans = active[active.length - 1] * active[active.length - 2];
+		return ans;
+	}
+	
+	public static int[] toInt(String[] s) {
+		int[] ret = new int[s.length];
+		for (int i = 0; i < s.length; i++) {
+			ret[i] = Integer.parseInt(s[i]);
+		}
+		return ret;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(ans());
+	}
+}
