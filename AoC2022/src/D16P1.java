0b61640c8131a0857c6b97bf278f40cf1b1697e0
tluo5458 <tluo5@DESKTOP-8IGITJ6.mynetworksettings.com>
2022-12-16 01:22:07 -0500

    Day 16 solve

---
+++ AoC2022/src/D16P1.java
@@ -0,0 +1,161 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class D16P1 {
+	/* 
+	 * Gigantic ups to the problem creator. Excellent problem, lots of fun to solve.
+	 */
+	
+	private static class Valve {
+		String name;
+		int flow;
+		String[] conn;
+		
+		public Valve(String s) {
+			String[] parse = s.split(" ");
+			name = parse[1];
+			flow = Integer.parseInt(parse[4].split("=")[1].split(";")[0]);
+			conn = new String[parse.length - 9];
+			for (int i = 9; i < parse.length; i++) {
+				conn[i - 9] = parse[i].substring(0, 2);
+			}
+		}
+		
+		public String toString() {
+			return name + ", flow rate " + flow + ", conns " + Arrays.toString(conn);
+		}
+	}
+	
+	private static class State {
+		HashSet<String> on;
+		int pressure;
+		int perTurn;
+		int turns;
+		String loc;
+		
+		public State() {
+			on = new HashSet<String>();
+			pressure = 0;
+			perTurn = 0;
+			turns = 0;
+			loc = "AA";
+		}
+		
+		public State moveAndOpen(Valve v, int dist) {
+			State s = new State();
+			s.on.addAll(on);
+			s.on.add(v.name);
+			s.perTurn = perTurn + v.flow;
+			s.turns = turns + dist;
+			if (s.turns >= 30) {
+				s.pressure = pressure + (30 - turns) * perTurn;
+			} else {
+				s.pressure = pressure + dist * perTurn;	
+			}
+			s.loc = v.name;
+			
+			return s;
+		}
+	}
+	
+	public static int ans() {
+		int ans = 0;
+		
+		File file = new File("inputs/d-16.txt");
+
+		HashMap<String, Valve> valves = new HashMap<String, Valve>();
+		ArrayList<String> pos = new ArrayList<String>();
+		
+		BufferedReader br;
+		String st;
+		try {
+			br = new BufferedReader(new FileReader(file));
+			while ((st = br.readLine()) != null) {
+				// we do a bit of parsing
+				Valve v = new Valve(st);
+				valves.put(v.name, v);
+				if (v.flow > 0) {
+					pos.add(v.name);
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		ArrayList<String> starts = new ArrayList<String>();
+		starts.addAll(pos);
+		starts.add("AA");
+		HashMap<String, HashMap<String, Integer>> grid = new HashMap<String, HashMap<String, Integer>>();
+		for (String valve : starts) {
+			HashMap<String, Integer> dists = new HashMap<String, Integer>();
+			HashSet<String> rem = new HashSet<String>();
+			rem.addAll(pos);
+			
+			HashSet<String> curr = new HashSet<String>();
+			curr.add(valve);
+			int dist = 0;
+			while (rem.size() > 0) {
+				HashSet<String> next = new HashSet<String>();
+				
+				for (String s : curr) {
+					for (String v : valves.get(s).conn) {
+						next.add(v);
+					}
+				}
+				dist++;
+				
+				for (String s : next) {
+					if (rem.contains(s)) {
+						rem.remove(s);
+						dists.put(s, dist + 1);
+					}
+				}
+				curr = next;
+			}
+			grid.put(valve, dists);
+		}
+		
+		// now bfs the ans
+		ArrayDeque<State> q = new ArrayDeque<State>();
+		q.add(new State());
+		while (!q.isEmpty()) {
+			State curr = q.pop();
+			if (curr.turns >= 30) {
+				ans = Math.max(ans, curr.pressure);
+				continue;
+			}
+			HashSet<String> canMove = new HashSet<String>();
+			canMove.addAll(grid.get(curr.loc).keySet());
+			for (String s : curr.on) {
+				canMove.remove(s);
+			}
+			for (String s : canMove) {
+				State n = curr.moveAndOpen(valves.get(s), grid.get(curr.loc).get(s));
+				q.add(n);
+			}
+		}
+		
+		return ans;
+	}
+	
+	public static int[] toInt(String[] s) {
+		int[] ret = new int[s.length];
+		for (int i = 0; i < s.length; i++) {
+			ret[i] = Integer.parseInt(s[i]);
+		}
+		return ret;
+	}
+	
+	public static void main(String[] args) {
+		long start = System.nanoTime();
+		System.out.println(ans());
+		long end = System.nanoTime();
+		System.out.println((end - start) / 1000 + " us");
+	}
+}
