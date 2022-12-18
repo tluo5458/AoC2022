1672da7339fa62ed9948571d0b9593723caa01ea
tluo5458 <tluo5@DESKTOP-8IGITJ6.mynetworksettings.com>
2022-12-18 01:15:11 -0500

    Day 17 solve (partial - will add Python files later)

---
+++ AoC2022/src/D17P1.java
@@ -0,0 +1,9 @@
+public class D17P1 {
+	/*
+	 * I did this one in Python. yea.
+	 */
+	
+	public static void main(String[] args) {
+		System.out.println(3209);
+	}
+}
---
+++ AoC2022/src/D17P2.java
@@ -0,0 +1,9 @@
+public class D17P2 {
+	/* 
+	 * Same as before
+	 */
+	
+	public static void main(String[] args) {
+		System.out.println(1580758017509L);
+	}
+}
